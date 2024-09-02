7264b63b66ce847ba5d6bae03532c9e927842954
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-09-02 17:27:30 -0500

    added actual content to portfolio about me page

--- Jordan-Reeve-Portfolio/src/pages/About.jsx
+++ Jordan-Reeve-Portfolio/src/pages/About.jsx
@@ -3,10 +3,12 @@ import '../assets/css/aboutme.css';
 import spaceVideo from '../assets/images/spacevideo.mp4';
 import ImgLeft from '../components/imgLeft';
 import ImgRight from '../components/ImgRight';
-const sampleText = "Nunc pharetra finibus est at efficitur. Praesent sed congue diam.Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.";
+const fAHText = "Father to two wonderful children and happily married.  I enjoy spending time with my wife and children.  My family and I enjoy playing games, watching movies, Cooking, Sight seeing and good company.  When I have a minute to my self I enjoy exploring new ideas, playing Video games, Shooting, Liftin weights,and enjoying a pipe or Cigar.";
+const sdevText ="As a software developer I am proficient in these technologies and languages: JavaScript, CSS, HTML, React, MongoDB, Sequelize, Handlebars, RESTFUL Api Development and MERN. Im always on the look out for learning new techniques and skills and enjoy sharing what I know as well.";
+const veteranText ="I am a Marine who served from the years 2009 to 2016. I operated the Light Armored Vehicle variants 25 and Log (the logistical variant) whilst serving my country I obtained my Bachelor of Science in Criminal Justice.  I am very thankful that I got the opportunity to serve my country and thankful to others who have done the same."
 import familyimg from '../assets/images/familypicture.jpg';
-
-import sampleImg from '../assets/images/Profile.png'
+import profilePhoto from'../assets/images/professional.png'
+import marineImg from '../assets/images/Profile.png'
 const AboutMe = () => {
   return (
     <div className='full-page-container' >
@@ -16,9 +18,9 @@ const AboutMe = () => {
       </video>
       <div className="content text-warning">
         <h1>About Me</h1>
-        <ImgLeft img={familyimg} title="Father and Husband" content={sampleText} key={1}/>
-        <ImgRight img={sampleImg} title="A Software Developer" content ={sampleText} key= {2} id="mid"/>
-        <ImgLeft img={sampleImg} title="Marine Corps Veteran" content={sampleText} key={3}/>
+        <ImgLeft img={familyimg} title="Father and Husband" content={fAHText} key={1}/>
+        <ImgRight img={profilePhoto} title="A Software Developer" content ={sdevText} key= {2} id="mid"/>
+        <ImgLeft img={marineImg} title="Marine Corps Veteran" content={veteranText} key={3}/>
       </div>
     </div>
   );
