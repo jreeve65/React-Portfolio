319ada0cf12d7b19c0b404ff1e2d047907c837f6
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-09-02 22:04:25 -0500

    updated portfolio cards to contain actual content

--- Jordan-Reeve-Portfolio/src/pages/Portfolio.jsx
+++ Jordan-Reeve-Portfolio/src/pages/Portfolio.jsx
@@ -3,6 +3,7 @@ import PortfolioCard from "../components/PortfolioCard";
 import background from "../assets/images/cpbackground.mp4";
 import bonfire from "../assets/images/bonfirelogo.png";
 import profileImg from "../assets/images/Profile.png";
+import nomad from"../assets/images/nomad.png";
 import "../assets/css/Portfolio.css"
 export default function Portfolio() {
   return (
@@ -16,51 +17,29 @@ export default function Portfolio() {
         projectImage={bonfire}
         projectTitle="Project-Bonfire"
         projectDescription="For more info"
-        projectLink="https://www.google.com/"
+        projectLink="https://bonfire-lgkj.onrender.com"
       />
       <PortfolioCard
         video={background}
-        projectImage={profileImg}
-        projectTitle="Bonfire"
+        projectImage={nomad}
+        projectTitle="Nomad"
         projectDescription="For more info"
-        projectLink="https://www.google.com/"
+        projectLink="https://breannacamacho.github.io/Nomad/"
       />
       <PortfolioCard
         video={background}
         projectImage={profileImg}
-        projectTitle="Bonfire"
+        projectTitle="Comming soon!"
         projectDescription="For more info"
         projectLink="https://www.google.com/"
       />
       
-       <div className="row">
-       <PortfolioCard
-        video={background}
-        projectImage={profileImg}
-        projectTitle="Bonfire"
-        projectDescription="For more info"
-        projectLink="https://www.google.com/"
-      /><PortfolioCard
-      video={background}
-      projectImage={profileImg}
-      projectTitle="Bonfire"
-      projectDescription="For more info"
-      projectLink="https://www.google.com/"
-    />
-    <PortfolioCard
-        video={background}
-        projectImage={profileImg}
-        projectTitle="Bonfire"
-        projectDescription="For more info"
-        projectLink="https://www.google.com/"
-      />
+    
        </div>
-      
-        
+      {/*add new row here for future projects */}        
      
       </div>
       </div>
       
-    </div>
   );
 }
