6abc59aed70ef927929a444c262345f20fed8410
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-10-07 11:42:46 -0500

    updated about me content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../assets/css/aboutme.css';
 import ImgLeft from '../components/ImgLeft';
 import ImgRight from '../components/ImgRight';
-const fAHText = "Father to two wonderful children and happily married.  I enjoy spending time with my wife and children.  My family and I enjoy playing games, watching movies, Cooking, Sight seeing and good company.  When I have a minute to my self I enjoy exploring new ideas, playing Video games, Shooting, Lifting weights,and enjoying a pipe or cigar.";
+const fAHText = "Father to two wonderful children and happily married.  I enjoy spending time with my wife and children.  My family and I enjoy playing games, watching movies, Cooking, Sight seeing and good company.  When I have a minute to my self I enjoy exploring new ideas, Marksmenship, Lifting weights, and playing guitar.";
 const sdevText ="As a software developer I am proficient in these technologies and languages: JavaScript, CSS, HTML, React, MongoDB, Sequelize, Handlebars, RESTFUL Api Development and MERN. Im always on the look out for learning new techniques and skills and enjoy sharing what I know as well.";
 const veteranText ="I am a Marine who served from the years 2009 to 2016. I operated the Light Armored Vehicle variants 25 and Log (the logistical variant) whilst serving my country I obtained my Bachelor of Science in Criminal Justice.  I am very thankful that I got the opportunity to serve my country and thankful to others who have done the same."
 import familyimg from '../assets/images/familypicture.jpg';
