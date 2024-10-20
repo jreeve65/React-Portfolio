5ccc7c5684b8adaa8f571e938c37aad841a9a308
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-10-20 16:21:39 -0500

    made splash page redirect after video ends.  made nav bar invisiable during the splash page

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -3,7 +3,11 @@ import { Link, useLocation } from 'react-router-dom';
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs() {
-  const currentPage = useLocation().pathname;
+  const currentPage = useLocation().pathname
+
+  if(currentPage ==='/'){
+    return null;
+  }
 
   return (
     <div className=' bg-dark text-warning nav-bar'>
--- src/pages/CoverPage.jsx
+++ src/pages/CoverPage.jsx
@@ -1,9 +1,18 @@
+import {useEffect} from "react"
+import{useNavigate} from "react-router-dom"
 import React from "react"
 import '../assets/css/cover.css';
 import {ReactTyped} from "react-typed";
 import spaceVideo from '../assets/images/saturnvideo.mp4';
 // const messageArray = ['Welcome to my Portfolio','Full Stack Development','Creative Design','Innovative Ideas','Agile Development']
 function CoverPage() {
+ const navigate = useNavigate();
+ useEffect(()=>{
+  const timer =setTimeout(()=>{
+    navigate('/About');
+  },20000);
+  return()=>clearTimeout(timer);
+ },[navigate]);
   return (
     <div className='full-page-container d-flex justify-content-center' >
       <video className="background-video" autoPlay muted loop>
@@ -11,7 +20,7 @@ function CoverPage() {
         Your browser does not support the video tag.
       </video>
       <h1 className="messages"><ReactTyped
-     strings ={['<','< Full Stack Development','< Creative Design','< Innovative Ideas','< Agile Development']} typeSpeed ={100}
+     strings ={['<','< Welcome','< Full Stack Development','< Creative Design','< Innovative Ideas','< Agile Development']} typeSpeed ={120}
      loop
      backSpeed ={20}
      cursorChar ="/>"
