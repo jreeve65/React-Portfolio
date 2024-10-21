e4c8f960bbf31664849ae5687c129ef0cb8e0a50
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-10-21 13:56:55 -0500

    added flipping card animation

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import innercard from "../assets/images/innercardback.mp4"
-const PortfolioCard = ({ backgroundImg, projectImage, projectTitle, projectDescription, projectLink }) => {
+import {useState} from 'react'
+import ReactCardFlip from "react-card-flip";
+const PortfolioCard = () => {
+    const [isFlipped,setIsFlipped] = useState(false);
+
+    function flipCard(){
+        setIsFlipped(!isFlipped);
+    }
+
     return (
-        // <div className="container mt-4">
 
-        <div className="card m-3">
-
-
-
-
-            <img src={projectImage} alt="Project" className="img-fluid my-1" />
-            <div className="inner-card card bg-dark text-dark">
-
-
-                <div className="card-body bg-light">
-
-                    <h5 className="card-title text-center">{projectTitle}</h5>
-
-                    <p className="card-text">{projectDescription}</p>
-                    <a href={projectLink} className="btn btn-primary">Click Here</a>
+   
+    
+        <div>
+            <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped} >
+                <div className="card" onMouseEnter ={flipCard}>
+                    <h1>Front</h1>
                 </div>
-            </div>
+                <div className="card card-back" onMouseLeave={flipCard}>
+                    <h1>Back</h1>
+                </div>
+            </ReactCardFlip>
         </div>
-        // </div>
+       
     );
 }
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,43 +3,24 @@ import PortfolioCard from "../components/PortfolioCard";
 // import background from "../assets/images/cpbackground.mp4";
 import bonfire from "../assets/images/bonfirelogo.png";
 import profileImg from "../assets/images/Profile.png";
-import nomad from"../assets/images/nomad.png";
+import nomad from "../assets/images/nomad.png";
 import "../assets/css/Portfolio.css"
 export default function Portfolio() {
   return (
     <div className="bg-dark text-warning mx-0 my-0" id="card-page">
-      
+
       <h1>Portfolio</h1>
       <div className="container-fluid d-flex justify-content-center mt-5">
-      <div className="row">
-      {/* <PortfolioCard
-        video={background}
-        projectImage={bonfire}
-        projectTitle="Project-Bonfire"
-        projectDescription="For more info"
-        projectLink="https://bonfire-lgkj.onrender.com"
-      />
-      <PortfolioCard
-        video={background}
-        projectImage={nomad}
-        projectTitle="Nomad"
-        projectDescription="For more info"
-        projectLink="https://breannacamacho.github.io/Nomad/"
-      />
-      <PortfolioCard
-        video={background}
-        projectImage={profileImg}
-        projectTitle="Comming soon!"
-        projectDescription="For more info"
-        projectLink="https://www.google.com/"
-      /> */}
-      
-    
-       </div>
-      {/*add new row here for future projects */}        
-     
+        <div className="row">
+          <PortfolioCard/>
+
+
+
+        </div>
+        {/*add new row here for future projects */}
+
       </div>
-      </div>
-      
+    </div>
+
   );
 }
