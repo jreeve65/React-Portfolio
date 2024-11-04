4d85bd59ab98e226392665b7336683d219fa3773
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-11-04 12:23:08 -0600

    added more projects to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,6 +4,8 @@ import cosmicQuest from "../assets/images/cq3.png";
 import profileImg from "../assets/images/Profile.png";
 import deathStar from "../assets/images/Death-star.png"
 import nomad from "../assets/images/nomad.png";
+import cardBack from"../assets/images/default-cardback.png"
+import notetaker from "../assets/images/note-taker2.png"
 import "../assets/css/Portfolio.css"
 const cqRepo="https://github.com/jreeve65/Cosmic-Quest"
 const cqApp ="https://cosmic-quest.onrender.com/"
@@ -31,13 +33,13 @@ export default function Portfolio() {
        </div>
        <div className="row d-flex justify-content-center">
         <div className="col">
-          <PortfolioCard projectImg={profileImg} Title="Note-Taker" Description={description}Githuburl="https://github.com/jreeve65/Notetaker" Appurl="https://note-taker-1b3e.onrender.com"></PortfolioCard>
+          <PortfolioCard projectImg={notetaker} Title="Note-Taker" Description={description}Githuburl="https://github.com/jreeve65/Notetaker" Appurl="https://note-taker-1b3e.onrender.com"></PortfolioCard>
         </div>
         <div className="col">
-          <PortfolioCard projectImg={profileImg}></PortfolioCard>
+          <PortfolioCard projectImg={cardBack} Title ="E-Commerce-API" Description={description} Githuburl="https://github.com/jreeve65/e-commerce-webserver"></PortfolioCard>
         </div>
         <div className="col">
-          <PortfolioCard projectImg={profileImg}></PortfolioCard>
+          <PortfolioCard projectImg={cardBack} Title="Social-Network-API" Description={description} Githuburl="https://github.com/jreeve65/social-networkAPI"></PortfolioCard>
         </div>
        </div>
        
