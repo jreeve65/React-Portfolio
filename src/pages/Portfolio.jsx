e2a4784a85a7822a12f77bf00eeb359346c65c88
Great Khaan <171518457+jreeve65@users.noreply.github.com>
2024-11-06 12:11:59 -0600

    replaced placeholder descriptions with actual descriptions

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -10,8 +10,13 @@ const cqRepo="https://github.com/jreeve65/Cosmic-Quest"
 const cqApp ="https://cosmic-quest.onrender.com/"
 const cqDescription ="A MERN stack application that features an exicting text-based choose your own adventure game set in a SciFi setting. Features include account creation, user authorization, and auto save."
 const nomadDescription = "A web application that taps into the google places api; designed to let one find high rated resturants near your chosen location.  Integrates google maps advanced markers which direct you to the venue's google maps page when clicked on."
-const employeeTrackerDes ="A command-line application that allows one to manage an employee database. App features node js, inquierer, and SQL with a little Star Wars Flair added for fun.   "
-const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."; 
+const employeeTrackerDes ="A command-line application that allows one to manage an employee database. App features node js, inquierer, and SQL with a little Star Wars Flair added for fun."
+const noteTakerDes ="A note taking app to help organize and plan your schedule.  Can be used to write reminders or jot down useful thoughts or ideas app uses Nodejs and Express."
+
+const ecommerceDes ="A  Api designed to be a used for e-commerce. Api utilizes Postgres, Sql, Express, JavaScript, and nodejs"
+
+const socialNetworkAPI ="A Api built to be used for A social media site,  Technologies used were Mongoose, Mongodb, Express, JavaScript, and nodejs."
+
 export default function Portfolio() {
   return (
     <div className="bg-dark text-warning mx-0 my-0" id="card-page">
@@ -32,13 +37,13 @@ export default function Portfolio() {
        </div>
        <div className="row d-flex justify-content-center">
         <div className="col">
-          <PortfolioCard projectImg={notetaker} Title="Note-Taker" Description={description}Githuburl="https://github.com/jreeve65/Notetaker" Appurl="https://note-taker-1b3e.onrender.com"></PortfolioCard>
+          <PortfolioCard projectImg={notetaker} Title="Note-Taker" Description={noteTakerDes}Githuburl="https://github.com/jreeve65/Notetaker" Appurl="https://note-taker-1b3e.onrender.com"></PortfolioCard>
         </div>
         <div className="col">
-          <PortfolioCard projectImg={cardBack} Title ="E-Commerce-API" Description={description} Githuburl="https://github.com/jreeve65/e-commerce-webserver"></PortfolioCard>
+          <PortfolioCard projectImg={cardBack} Title ="E-Commerce-API" Description={ecommerceDes} Githuburl="https://github.com/jreeve65/e-commerce-webserver"></PortfolioCard>
         </div>
         <div className="col">
-          <PortfolioCard projectImg={cardBack} Title="Social-Network-API" Description={description} Githuburl="https://github.com/jreeve65/social-networkAPI"></PortfolioCard>
+          <PortfolioCard projectImg={cardBack} Title="Social-Network-API" Description={socialNetworkAPI} Githuburl="https://github.com/jreeve65/social-networkAPI"></PortfolioCard>
         </div>
        </div>
        
